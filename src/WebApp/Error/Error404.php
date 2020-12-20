b979d5114da5eaeb3fbb4f1acd0682cfb069a4c2
technicalguru <github@ralph-schuster.eu>
2020-12-20 11:00:55 +0100

    Add TODO

--- src/WebApp/Error/Error404.php
+++ src/WebApp/Error/Error404.php
@@ -16,6 +16,7 @@ class Error404 extends \WebApp\Page {
 		header('HTTP/1.1 404 Not Found');
 		$rc = new \WebApp\Component\MainContent($this);
 		$rc->addClass('text-center');
+		// TODO Add this text in white on black image with broken glass
 		new \WebApp\Component\Title($rc, 'page404_title');
 		new \WebApp\Component\Subtitle($rc, 'page404_subtitle');
 		$p = new \WebApp\Component\Paragraph($rc, 'page404_description');
