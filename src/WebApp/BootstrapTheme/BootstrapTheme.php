28eefed7380285a9ed76cb2fdd0095611969681a
technicalguru <github@ralph-schuster.eu>
2020-12-30 10:26:11 +0100

    Add features for multiselect and datepicker

--- src/WebApp/BootstrapTheme/BootstrapTheme.php
+++ src/WebApp/BootstrapTheme/BootstrapTheme.php
@@ -5,6 +5,9 @@ namespace WebApp\BootstrapTheme;
 /** A basic theme that has no layout at all */
 class BootstrapTheme extends \WebApp\DefaultTheme\DefaultTheme {
 
+	public const MULTISELECT = 'multiselect';
+	public const DATEPICKER  = 'datepicker';
+
 	public function __construct($app) {
 		parent::__construct($app);
 	}
--- src/WebApp/BootstrapTheme/DefaultLayout.php
+++ src/WebApp/BootstrapTheme/DefaultLayout.php
@@ -18,19 +18,24 @@ class DefaultLayout extends \WebApp\Layout {
 	protected function renderLinks() {
 		$webroot = $this->app->request->webRoot;
 		$rc  = '<link rel="stylesheet" href="'.$webroot.FontAwesome::getUri().'" rel="stylesheet" type="text/css">'.
-		       '<link rel="stylesheet" href="'.$webroot.Bootstrap::getCssUri().'" rel="stylesheet" type="text/css">'.
-		       '<link rel="stylesheet" href="'.Utils::getCssBasePath(TRUE).'/bootstrap-datepicker.min.css" rel="stylesheet" type="text/css">'.
-		       '<style>'.
-		          '.dropdown-item.nav-link {'.
-		             'padding: .25rem 1.5rem !important;'.
-		          '}'.
-		          '.navbar-dark .dropdown-item.nav-link:hover,'.
-		          '.navbar-dark .dropdown-item.nav-link:active,'.
-		          '.navbar-dark .dropdown-item.nav-link:focus {'.
-		             'color: #333333 !important;'.
-		             'background-color: #f8f9fa !important;'.
-		          '}'.
-		        '</style>';
+		       '<link rel="stylesheet" href="'.$webroot.Bootstrap::getCssUri().'" rel="stylesheet" type="text/css">';
+		if ($this->theme->hasFeature(BootstrapTheme::DATEPICKER)) {
+			$rc .= '<link rel="stylesheet" href="'.Utils::getCssBasePath(TRUE).'/bootstrap-datepicker.min.css" rel="stylesheet" type="text/css">'.
+		           '<style>'.
+		              '.dropdown-item.nav-link {'.
+		                 'padding: .25rem 1.5rem !important;'.
+		              '}'.
+		              '.navbar-dark .dropdown-item.nav-link:hover,'.
+		              '.navbar-dark .dropdown-item.nav-link:active,'.
+		              '.navbar-dark .dropdown-item.nav-link:focus {'.
+		                 'color: #333333 !important;'.
+		                 'background-color: #f8f9fa !important;'.
+		              '}'.
+		           '</style>';
+		}
+		if ($this->theme->hasFeature(BootstrapTheme::MULTISELECT)) {
+			$rc .= '<link rel="stylesheet" href="'.Utils::getCssBasePath(TRUE).'/filter-multi-select.css" rel="stylesheet" type="text/css">';
+		}
 		$rc .= parent::renderLinks();
 		return $rc;
 	}
@@ -138,6 +143,9 @@ class DefaultLayout extends \WebApp\Layout {
 		       '<script src="'.Utils::getJavascriptBasePath(TRUE).'/webapp.js"></script>'.
 		       '<script src="'.Utils::getJavascriptBasePath(TRUE).'/utils.js"></script>';
 		$rc .= parent::renderJavascript();
+		if ($this->theme->hasFeature(BootstrapTheme::MULTISELECT)) {
+			'<script src="'.Utils::getJavascriptBasePath(TRUE).'/filter-multi-select-bundle.js"></script>';
+		}
 		return $rc;
 	}
 
