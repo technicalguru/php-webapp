95485b61bcefde3de6b5189579b370a05a04df7c
technicalguru <github@ralph-schuster.eu>
2021-01-09 11:55:36 +0100

    Upgrade to new technicalguru/database version

--- src/WebApp/DataModel/LogDAO.php
+++ src/WebApp/DataModel/LogDAO.php
@@ -6,29 +6,25 @@ use WebApp\WebAppException;
 
 class LogDAO extends \TgDatabase\DAO {
 
-	public function __construct($database) {
-		parent::__construct($database, '#__error_log', 'WebApp\\DataModel\\Log');
-		$this->initialize();
+	public function __construct($database, $checkTable = FALSE) {
+		parent::__construct($database, '#__error_log', 'WebApp\\DataModel\\Log', 'uid', $checkTable);
 	}
 
-	protected function initialize() {
-		// Check the table existence
-		$res = $this->database->query('SELECT * FROM '.$this->tableName);
+	public function createTable() {
+		// Try to create
+		$sql =
+			'CREATE TABLE '.$this->database->quoteName($this->tableName).' ('.
+				'`uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,'.
+				'`log_date` datetime NOT NULL,'.
+				'`log_text` text COLLATE utf8mb4_bin NOT NULL,'.
+				'`log_sent` datetime DEFAULT NULL,'.
+				'PRIMARY KEY (`uid`)'.
+			') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin';
+		$res = $this->database->query($sql);
 		if ($res === FALSE) {
-			// Try to create
-			$sql =
-				'CREATE TABLE '.$this->database->quoteName($this->tableName).' ('.
-					'`uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,'.
-					'`log_date` datetime NOT NULL,'.
-					'`log_text` text COLLATE utf8mb4_bin NOT NULL,'.
-					'`log_sent` datetime DEFAULT NULL,'.
-					'PRIMARY KEY (`uid`)'.
-				') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin';
-			$res = $this->database->query($sql);
-			if ($res === FALSE) {
-				throw new WebAppException('Cannot create log table: '.$this->database->error());
-			}
+			throw new WebAppException('Cannot create log table: '.$this->database->error());
 		}
+		return TRUE;
 	}
 
 }
--- src/WebApp/DataModel/UserRoleDAO.php
+++ src/WebApp/DataModel/UserRoleDAO.php
@@ -6,30 +6,26 @@ use WebApp\WebAppException;
 
 class UserRoleDAO extends \TgDatabase\DAO {
 
-	public function __construct($database, $modelClass = NULL) {
-		parent::__construct($database, '#__user_roles', $modelClass != NULL ? $modelClass : 'WebApp\\DataModel\\UserRole');
-		$this->initialize();
+	public function __construct($database, $modelClass = NULL, $checkTable = FALSE) {
+		parent::__construct($database, '#__user_roles', $modelClass != NULL ? $modelClass : 'WebApp\\DataModel\\UserRole', 'uid', $checkTable);
 	}
 
-	protected function initialize() {
-		// Check the table existence
-		$res = $this->database->query('SELECT * FROM '.$this->tableName);
+	public function createTable() {
+		// Try to create
+		$sql =
+			'CREATE TABLE '.$this->database->quoteName($this->tableName).' ('.
+				'`uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,'.
+				'`name` varchar(50) COLLATE utf8mb4_bin NOT NULL,'.
+				'`privileges` text COLLATE utf8mb4_bin NOT NULL,'.
+				'`is_active` int(1) DEFAULT 1 NOT NULL,'.
+				'PRIMARY KEY (`uid`), '.
+				'UNIQUE(`name`) '.
+			') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin';
+		$res = $this->database->query($sql);
 		if ($res === FALSE) {
-			// Try to create
-			$sql =
-				'CREATE TABLE '.$this->database->quoteName($this->tableName).' ('.
-					'`uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,'.
-					'`name` varchar(50) COLLATE utf8mb4_bin NOT NULL,'.
-					'`privileges` text COLLATE utf8mb4_bin NOT NULL,'.
-					'`is_active` int(1) DEFAULT 1 NOT NULL,'.
-					'PRIMARY KEY (`uid`), '.
-					'UNIQUE(`name`) '.
-				') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin';
-			$res = $this->database->query($sql);
-			if ($res === FALSE) {
-				throw new WebAppException('Cannot create user roles table: '.$this->database->error());
-			}
+			throw new WebAppException('Cannot create user roles table: '.$this->database->error());
 		}
+		return TRUE;
 	}
 
 }
