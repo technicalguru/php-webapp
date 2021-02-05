18ab14c34120a8902666865fc2e7228926fef8e7
technicalguru <github@ralph-schuster.eu>
2021-02-05 10:57:40 +0100

    Add empty option

--- src/WebApp/Component/Select.php
+++ src/WebApp/Component/Select.php
@@ -8,7 +8,16 @@ class Select extends FormElement {
 
 	public function __construct($parent, $id, $options = array(), $value = null) {
 		parent::__construct($parent, $id, $value);
-		$this->options = $options;
+		$this->options     = $options;
+		$this->emptyOption = NULL;
+	}
+
+	public function setEmptyOption($value) {
+		$this->emptyOption = $value;
+	}
+
+	public function getEmptyOption() {
+		return $this->emptyOption;
 	}
 
 	public function getOptions() {
--- src/WebApp/DefaultTheme/SelectRenderer.php
+++ src/WebApp/DefaultTheme/SelectRenderer.php
@@ -13,12 +13,21 @@ class SelectRenderer extends \WebApp\Renderer {
 	public function render() {
 		$rc    = $this->renderStartTag('select');
 		$value = $this->component->getAttribute('value', TRUE);
+		$emptyOption = $this->component->getEmptyOption();
+		\TgLog\Log::debug('('.$this->component->getId().') emptyOption='.$emptyOption.'  NULL='.($emptyOption === NULL ? 'YES' : 'NO'));
+		if ($emptyOption !== NULL) {
+			$rc .= self::renderOption('', $emptyOption, $value == '');
+		}
 		foreach ($this->component->getOptions() AS $key => $label) {
-			$selected = $value == $key ? ' selected="selected"' : '';
-			$rc .= '<option value="'.htmlentities($key).'"'.$selected.'>'.I18N::_($label).'</option>';
+			$rc .= self::renderOption($key, $label, $value == $key);
 		}
 		$rc .= $this->renderEndTag('select');
 		return $rc;
+	}
+
+	protected static function renderOption($key, $label, $isSelected) {
+		$selected = $isSelected ?  ' selected="selected"' : '';
+		return '<option value="'.htmlentities($key).'"'.$selected.'>'.I18N::_($label).'</option>';
 	}
 }
 
