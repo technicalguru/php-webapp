80b326444ae75899a77ebb9d7b4a260ec99eaa85
technicalguru <github@ralph-schuster.eu>
2021-02-18 09:20:34 +0100

    Fix variable name

--- src/WebApp/WebAppException.php
+++ src/WebApp/WebAppException.php
@@ -5,7 +5,7 @@ namespace WebApp;
 class WebAppException extends \Exception {
 
 	public function __construct(string $message="", int $code=0, \Throwable $previous=NULL) {
-		parent::__construct($message, $code, $prevois);
+		parent::__construct($message, $code, $previous);
 	}
 
 }
