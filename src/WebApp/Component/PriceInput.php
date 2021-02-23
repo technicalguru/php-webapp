3ef1d3b28dc95f19c5eaa5dfa61a9aa31d17ee94
technicalguru <github@ralph-schuster.eu>
2021-02-23 15:13:33 +0100

    Fix formatting

--- src/WebApp/Component/PriceInput.php
+++ src/WebApp/Component/PriceInput.php
@@ -2,10 +2,12 @@
 
 namespace WebApp\Component;
 
+use TgUtils\FormatUtils;
+
 class PriceInput extends NumberInput {
 
 	public function __construct($parent, $id, $value = null, $currency = NULL) {
-		parent::__construct($parent, $id, $value != NULL ? sprintf('%01.2f', $value) : '0.00');
+		parent::__construct($parent, $id, FormatUtils::formatPrice($value != NULL ? $value : 0, '', NULL, ''));
 		$this->currency = $currency;
 		$this->setStep(0.01);
 		$this->setMin(0);
