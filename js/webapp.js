6552d56249c8d9e95f755a63a7946f56767fcacf
technicalguru <github@ralph-schuster.eu>
2021-03-03 10:48:44 +0100

    Fixed backdrop removal for spinner

--- js/webapp.js
+++ js/webapp.js
@@ -120,12 +120,13 @@ WebApp.prototype.DELETE = function(url, ajaxController) {
 
 WebApp.prototype.showSpinner = function(label) {
 	if (typeof label === undefined) label = this.i18n('pleaseWait');
-	jQuery('<div id="page-blocker" class="modal" tabindex="-1"><div class="vertical-center"><div class="spinner-border" role="status"><span class="sr-only">'+label+'</span></div></div></div><div class="modal-backdrop show"></div>').appendTo('body');
+	jQuery('<div id="page-blocker" class="modal" tabindex="-1"><div class="vertical-center"><div class="spinner-border" role="status"><span class="sr-only">'+label+'</span></div></div></div><div id="page-blocker-backdrop" class="modal-backdrop show"></div>').appendTo('body');
 	jQuery('#page-blocker').show();
 };
 
 WebApp.prototype.hideSpinner = function() {
 	jQuery('#page-blocker').remove();
+	jQuery('#page-blocker-backdrop').remove();
 };
 
 // Register an I18N object for translations
