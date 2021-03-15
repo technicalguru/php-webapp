383eef4fdbe52d29fd13eece78a61bdc24a4a040
technicalguru <github@ralph-schuster.eu>
2021-03-15 14:53:10 +0100

    Render messages in MainContent by default

--- src/WebApp/Component/MainContent.php
+++ src/WebApp/Component/MainContent.php
@@ -4,9 +4,13 @@ namespace WebApp\Component;
 
 class MainContent extends Div {
 
-	public function __construct($parent, $child = NULL) {
+	public function __construct($parent, $child = NULL, $renderMessages = TRUE) {
 		parent::__construct($parent, $child);
 		$this->addClass('content-main');
+		if ($renderMessages) {
+			$msg = new SystemMessages($this);
+			$msg->addClass('mb-4');
+		}
 	}
 
 }
