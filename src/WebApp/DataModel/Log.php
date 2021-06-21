00069a00dc5a3b71625ab91cc6d3462057a56b5b
technicalguru <github@ralph-schuster.eu>
2021-06-21 15:55:35 +0200

    Save log messages as object now

--- src/WebApp/DataModel/Log.php
+++ src/WebApp/DataModel/Log.php
@@ -5,6 +5,9 @@ namespace WebApp\DataModel;
 class Log {
 
 	public function __construct() {
+		if (is_string($this->log_text)) {
+			if (substr($this->log_text, 0, 1) == '{') $this->log_text = json_decode($this->log_text);
+		}
 	}
 
 }
