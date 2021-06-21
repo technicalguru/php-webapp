d7910e90fd314a93277492c2887b09bd10084db0
technicalguru <github@ralph-schuster.eu>
2021-06-21 16:26:21 +0200

    methods for UI

--- src/WebApp/DataModel/Log.php
+++ src/WebApp/DataModel/Log.php
@@ -8,4 +8,30 @@ class Log {
 		if (is_string($this->log_text)) $this->log_text = json_decode($this->log_text);
 	}
 
+	public function asText() {
+		$rc = '';
+		foreach (array('error', 'warn', 'info', 'debug') AS $sev) {
+			if (isset($this->log_text->$sev)) {
+				foreach ($this->log_text->$sev AS $msg) {
+					$rc .= '['.strtoupper($sev).'] '.$msg."\n";
+				}
+			}
+		}
+		return $rc;
+	}
+
+	public function getPreview() {
+		$rc = '';
+		foreach (array('error', 'warn', 'info', 'debug') AS $sev) {
+			if (isset($this->log_text->$sev)) {
+				foreach ($this->log_text->$sev AS $msg) {
+					$rc = '['.strtoupper($sev).'] '.$msg;
+					break 2;
+				}
+			}
+		}
+		if (strlen($rc) > 100) $rc = substr($rc, 0, 100);
+		return $rc;
+	}
+		
 }
