708b0f9d6f1b22302843e6598077edae2d2b97c0
technicalguru <github@ralph-schuster.eu>
2021-10-10 11:33:49 +0200

    Add LinkBuilderService and abstract implementation

---
+++ src/WebApp/Service/AbstractLinkBuilder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WebApp\Service;
+
+/** Descendants only need to declare methods like getViewLink(), getEditLink() etc. */
+class AbstractLinkBuilder implements \TgUtils\LinkBuilder {
+
+	public function __construct($app) {
+		$this->app = $app;
+	}
+
+	public function getLink($subject, $action = \TgUtils\LinkBuilder::VIEW, $params = NULL) {
+		if ($action == '') return '#'; // To break unwanted loops
+		$methodName = 'get'.ucfirst($action).'Link';
+		if (method_exists($this, $methodName)) {
+			return call_user_func(array($this, $methodName), $subject, $params);
+		}
+		return '#';
+	}
+}
+
