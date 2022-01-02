54934683a630726f9971b35a58b5006ab84511fb
technicalguru <github@ralph-schuster.eu>
2022-01-02 16:30:39 +0100

    Fix group padding

--- src/WebApp/Component/FormCheckGroup.php
+++ src/WebApp/Component/FormCheckGroup.php
@@ -6,6 +6,7 @@ abstract class FormCheckGroup extends CombinedFormElement {
 
 	public function __construct($parent, $id, $values = NULL) {
 		parent::__construct($parent, $id, $values);
+		$this->setStyle('padding-top', '0.5rem');
 	}
 
 }
